bd856a1aec0914a28d4cf1bb55ac9c36dbf1224e
Jonathan Duck <duckbrain30@gmail.com>
2017-07-29 06:32:13 -0700

    Committing some attempts. Not working

--- ldssa/ldssa.go
+++ ldssa/ldssa.go
@@ -10,26 +10,18 @@ func init() {
 	/*
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			defer web.HandleError(w, r)
-			lib.DataDirectory = "/storage/emulated/0/.ldss"
-			dir, err := filepath.Abs(lib.DataDirectory)
-			w.Write([]byte(dir))
-			if err != nil {
-				panic(err)
-			}
 
-			w.Write([]byte("\n"))
+			user, _ := user.Current()
 
-			lang, err := lib.LookupLanguage("eng")
-			if err != nil {
-				panic(err)
-			}
-			w.Write([]byte(lang.String()))
+			w.Write([]byte(user.HomeDir))
 		})
-	*/
+		//*/
 
+	//*
 	lang, err := lib.LookupLanguage("eng")
 	if err != nil {
 		panic(err)
 	}
 	web.Handle(lang)
+	//*/
 }
