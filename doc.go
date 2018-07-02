b35662d93252f140dc1cf5b517ecd62f46e2d203
Jonathan Duck <duckbrain30@gmail.com>
2018-07-02 06:42:53 -0700

    Consolodate and remove unused code in root

--- doc.go
+++
@@ -1,5 +0,0 @@
-// ldss is a program to download and read the scriptures from the Church of Jesus Christ
-// of Latter-day Saints in a variety of formats. It contains a web server, GUI, and
-// command line interface. The content parsing and lookup is implemented in the ldss/lib
-// subpackage under this package.
-package main
