4b7098054b8973967540083a772bcab733b5029e
Jonathan Duck <duckbrain30@gmail.com>
2019-09-18 18:17:14 -0700

    Add basic android project

---
+++ android/ldssa.go
@@ -0,0 +1,41 @@
+package ldssandroid
+
+import (
+	"fmt"
+
+	"github.com/duckbrain/ldss/internal/web"
+	"github.com/duckbrain/ldss/lib"
+)
+
+func Test() {
+	fmt.Println("Hello")
+}
+
+func init() {
+	lib.DataDirectory = "/storage/emulated/0/.ldss"
+	/*
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			defer web.HandleError(w, r)
+			lib.DataDirectory = "/storage/emulated/0/.ldss"
+			dir, err := filepath.Abs(lib.DataDirectory)
+			w.Write([]byte(dir))
+			if err != nil {
+				panic(err)
+			}
+
+			w.Write([]byte("\n"))
+
+			lang, err := lib.LookupLanguage("eng")
+			if err != nil {
+				panic(err)
+			}
+			w.Write([]byte(lang.String()))
+		})
+	*/
+
+	lang := lib.LookupLanguage("en")
+	if lang == nil {
+		panic("Language not found")
+	}
+	web.Handle(lang)
+}
---
+++ android/webapp.go
@@ -0,0 +1,37 @@
+package ldssandroid
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+var server = &http.Server{
+	Addr:           "127.0.0.1:0",
+	Handler:        http.DefaultServeMux,
+	ReadTimeout:    10 * time.Second,
+	WriteTimeout:   10 * time.Second,
+	MaxHeaderBytes: 1 << 20,
+}
+
+//Start is called by the native portion of the webapp to start the web server.
+//It returns the server root URL (without the trailing slash) and any errors.
+func Start() string {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	go func() {
+		err = server.Serve(listener)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	return listener.Addr().String()
+}
+
+//Stop is called by the native portion of the webapp to stop the web server.
+func Stop() {
+	server.Close()
+}
